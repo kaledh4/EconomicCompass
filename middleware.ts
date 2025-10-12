a06a00e110f02ea5c10edb15de373ed8fb86fa09
Mamdouh Albrak <muxd22@gmail.com>
2025-10-12 14:24:27 +0000

    MMMMMMMMMMM

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import createMiddleware from 'next-intl/middleware';
- 
+
 export default createMiddleware({
-  locales: ['en', 'ar'],
- 
+  locales: ['en', 'es'],
   defaultLocale: 'en'
 });
- 
+
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-import withNextIntl from 'next-intl/plugin';
-
-const withNextIntlConfig = withNextIntl();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default withNextIntlConfig(nextConfig);
