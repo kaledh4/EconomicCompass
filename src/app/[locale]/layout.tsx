b8b50e864d8245ecebe5d36d652a89401b17f062
Mamdouh Albrak <muxd22@gmail.com>
2025-10-12 14:25:45 +0000

    MMMMMM

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from 'next';
-import './globals.css';
+import '../../globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 import { SettingsProvider } from '@/contexts/settings-context';
