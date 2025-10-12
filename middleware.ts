1157f5743ea5eeac6490974bb9c708a088cc86a1
Mamdouh Albrak <muxd22@gmail.com>
2025-10-12 14:03:59 +0000

    https://github.com/kaledh4/Economic-Compass

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
+import { locales, defaultLocale } from './i18n';
  
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['en', 'ar'],
- 
-  // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale,
+  locales,
 });
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(ar|en)/:path*']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,14 @@
-import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
+import { notFound } from 'next/navigation';
  
 // Can be imported from a shared config
-const locales = ['en', 'ar'];
+export const locales = ['en', 'ar'];
+export const defaultLocale = 'en';
  
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
- 
+
   return {
     messages: (await import(`./locales/${locale}.json`)).default
   };
